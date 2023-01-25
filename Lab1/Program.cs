cf534f5d74997e55fd4388a419b5a7e9b3a6396a
SirYeast <mkorzhan@outlook.com>
2023-01-25 15:16:49 -0600

    Start lab 1

---
+++ Lab1/Program.cs
@@ -0,0 +1,64 @@
+﻿using Lab1;
+
+Dictionary<int, int> coinStorage = new()
+{
+    {20, 2},
+    {15, 1},
+    {10, 3},
+    {5, 3},
+    {2, 4},
+    {1, 5}
+};
+
+Dictionary<Product, int> inventory = new()
+{
+    {new Product("Ruffles Original Chips", 3, "A1"), 1},
+    {new Product("Skittles", 2, "A2"), 3},
+    {new Product("Doritos Nacho Cheese", 4, "A3"), 2},
+    {new Product("Pretzels", 1, "A4"), 0}
+};
+
+VendingMachine vendingMachine = new(4832541, coinStorage, inventory);
+Console.WriteLine(vendingMachine.StockItem(new Product("Ruffles Original Chips", 3, "A1"), 4));
+Console.WriteLine(vendingMachine.StockFloat(5, 4));
+Console.WriteLine();
+
+string? input = "";
+string moneyInput = "";
+string itemInput = "";
+
+int phase = 0;
+
+while (input != "CANCEL")
+{
+    if (phase == 0)
+    {
+        Console.WriteLine("Please enter money amount (or 'CANCEL'):");
+        input = Console.ReadLine();
+
+        if (!string.IsNullOrWhiteSpace(input))
+        {
+            moneyInput = input;
+            phase++;
+        }
+    }
+    else if (phase == 1)
+    {
+        Console.WriteLine("Please enter item code (or 'CANCEL'):");
+        input = Console.ReadLine();
+
+        if (!string.IsNullOrWhiteSpace(input))
+        {
+            itemInput = input.ToUpper();
+            phase++;
+        }
+    }
+    else
+    {
+        int money = int.Parse(moneyInput);
+
+        string message = vendingMachine.VendItem(itemInput, new List<int>() {money});
+        Console.WriteLine(message);
+        break;
+    }
+}
---
+++ Lab1/VendingMachine.cs
@@ -0,0 +1,111 @@
+﻿using System.Text;
+
+namespace Lab1
+{
+    public class VendingMachine
+    {
+        public int SerialNumber { get; }
+        public Dictionary<int, int> MoneyFloat { get; }
+        public Dictionary<Product, int> Inventory { get; }
+
+        public VendingMachine(int serialNumber, Dictionary<int, int> moneyFloat, Dictionary<Product, int> inventory)
+        {
+            SerialNumber = serialNumber;
+            MoneyFloat = moneyFloat;
+            Inventory = inventory;
+        }
+
+        public string StockItem(Product product, int quantity)
+        {
+            Product? existingProduct = null;
+
+            foreach (Product p in Inventory.Keys)
+            {
+                if (p.Code == product.Code)
+                {
+                    existingProduct = p;
+                    break;
+                }
+            }
+
+            if (existingProduct != null)
+            {
+                Inventory[existingProduct] += quantity;
+            }
+            else
+            {
+                existingProduct = product;
+                Inventory[existingProduct] = quantity;
+            }
+
+            return $"Name: {product.Name} | Code: {product.Code} | Price: ${product.Price} | Quantity: {Inventory[existingProduct]}";
+        }
+
+        public string StockFloat(int moneyDenomination, int quantity)
+        {
+            if (MoneyFloat.ContainsKey(moneyDenomination))
+            {
+                MoneyFloat[moneyDenomination] += quantity;
+            }
+
+            return $"Float stock: {string.Join(',', MoneyFloat)}";
+        }
+
+        public string VendItem(string code, List<int> money)
+        {
+            Product? selectedProduct = null;
+
+            foreach (Product product in Inventory.Keys)
+            {
+                if (code == product.Code)
+                {
+                    selectedProduct = product;
+                    break;
+                }
+            }
+
+            if (selectedProduct == null)
+                return $"Error, no item with code '{code}' exists";
+
+            if (Inventory[selectedProduct] == 0)
+                return "Error, item is out of stock";
+
+            int totalMoney = money.Sum();
+
+            if (totalMoney < selectedProduct.Price)
+                return $"Error, insufficient money provided. That item costs ${selectedProduct.Price}";
+
+            int change = totalMoney - selectedProduct.Price;
+            Dictionary<int, int> coinReturn = new();
+
+            while (change > 0)
+            {
+                int greatest = 0;
+
+                foreach (KeyValuePair<int, int> pair in MoneyFloat)
+                    if (pair.Value > 0 && greatest < pair.Key && change >= pair.Key)
+                        greatest = pair.Key;
+
+                if (greatest == 0)
+                    return "Operation cancelled, insufficient change";
+
+                MoneyFloat[greatest]--;
+                change -= greatest;
+
+                coinReturn[greatest] = coinReturn.ContainsKey(greatest) ? coinReturn[greatest] + 1 : 1;
+            }
+
+            if (coinReturn.Count > 0)
+            {
+                Console.WriteLine("Returned coins:");
+
+                foreach (KeyValuePair<int, int> pair in coinReturn)
+                    Console.WriteLine($"${pair.Key}: {pair.Value}");
+            }
+
+            Inventory[selectedProduct]--;
+
+            return $"You have successfully purchased {selectedProduct.Name} for ${selectedProduct.Price}";
+        }
+    }
+}
